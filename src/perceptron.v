a36c7d8d0a59324de94d6b03eac540a611a75cc1
Clarence Chan <chan.clarence99@gmail.com>
2024-10-31 16:36:21 -0700

    fix more syntax errors

--- src/perceptron.v
+++ src/perceptron.v
@@ -7,7 +7,7 @@ module perceptron (
     input  wire[7:0]  threshold, // activation threshold
     output wire       result     // actual result of perceptron calculation
 );
-    reg[7:0] weights; // how to get float weights??
+    reg[7:0] weights; // how to get float weights
     reg[7:0] net_input;
     wire error;
 
@@ -15,18 +15,20 @@ module perceptron (
         if (!reset) begin
             result <= 0;
             net_input <= 0;
-            weights <= 8'b01100101
+            // SGD
+            weights <= 8'b01100101; //temp
         end else begin
             result <= (net_input >= threshold); // evaluate perceptron
+            // error calculation
         end
     end
 
     integer i;
 
-    for (i = 0; i < d'8; i = i + 1) // based on 8 inputs
+    for (i = 0; i < 8; i = i + 1) // based on 8 inputs
     begin
-        // integer w = d'8 * i;
-        net_input = net_input + {8{in[i]}} * weights[i]; // multiply inputs with corresponding weights
+        // integer w = 8 * i;
+        net_input[7:0] <= net_input[7:0] + in[i] * weights[i]; // multiply inputs with corresponding weights
     end
 
 endmodule
